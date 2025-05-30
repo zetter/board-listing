45faacf069e1b5158f130e63677f8e0f53016d50
Chris Zetter <zetter@users.noreply.github.com>
2025-05-30 14:19:36 +0100

    Failing test case

---
+++ combine.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "bun:test";
+import { $ } from "bun";
+
+const currentDir = import.meta.dir;
+
+test("combines input files to expected output", async () => {
+    const output = await $`${currentDir}/combine ${currentDir}/test_data/example_boards`.text();
+    const parsedOutput = JSON.parse(output);
+
+    const file = Bun.file(`${currentDir}/test_data/output.json`);
+    const expectedOutput = await file.json();
+
+    expect(parsedOutput).toEqual(expectedOutput);
+});
