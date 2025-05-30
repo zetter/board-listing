4f49781d7acd93bcda71f80f459c0a194a942b22
Chris Zetter <zetter@users.noreply.github.com>
2025-05-30 15:44:36 +0100

    Fix program on windows
    Don't directly use the script with the shebang and make sure paths are built in a cross-platform way.

--- combine.test.ts
+++ combine.test.ts
@@ -1,13 +1,15 @@
 import { expect, test } from "bun:test";
+import { join } from "path";
 import { $ } from "bun";
 
 const currentDir = import.meta.dir;
 
 test("combines input files to expected output", async () => {
-    const output = await $`${currentDir}/combine ${currentDir}/test_data/example_boards`.text();
+
+    const output = await $`bun run combine ${join(currentDir, 'test_data', 'example_boards')}`.text();
     const parsedOutput = JSON.parse(output);
 
-    const file = Bun.file(`${currentDir}/test_data/output.json`);
+    const file = Bun.file(join(currentDir, 'test_data', 'output.json'));
     const expectedOutput = await file.json();
 
     expect(parsedOutput).toEqual(expectedOutput);
